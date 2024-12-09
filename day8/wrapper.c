846c4ec542989e716b41b378e3034fbfeef80e8c
Matt Stock <stock@csgeeks.org>
2024-12-08 23:20:08 -0500

    working part 1

--- day8/wrapper.c
+++ day8/wrapper.c
@@ -9,9 +9,28 @@ unsigned char *map;
 unsigned char *paint;
 unsigned int mapsize;
 
+extern void antiseek();
+extern int yoff(unsigned char *pos1, unsigned char *pos2);
+extern int xoff(unsigned char *pos1, unsigned char *pos2);
+
+extern unsigned int cols, rows;
+
+// so much easier to do this in C!
+void debugchat(unsigned char *a, unsigned char *b) {
+  printf("match %c (%d, %d) -> (%d, %d)\n",
+	 *a,
+	 xoff(map, a), yoff(map, a),
+	 xoff(map, b), yoff(map, b));
+  printf("  offset (%d, %d)\n", xoff(map,b)-xoff(map,a), yoff(map,b)-yoff(map,a));
+}
+
+void printmap() {
+  printf("%s", paint);
+}
+
 int main(int argc, char **argv) {
   int fd;
-  
+  int count = 0;
   char str[60];
   
   if (argc != 2) {
@@ -34,7 +53,16 @@ int main(int argc, char **argv) {
   paint[mapsize] = '\0';
   close(fd);
 
+  antiseek();
+
+  printf("%s\n", paint);
+
   
+  for (int i=0; i < mapsize; i++)
+    count += (paint[i] == '#');
+
+  printf("antinodes = %d\n", count);
   free(map);
+  free(paint);
   exit(0);
 }
